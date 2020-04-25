c8da40169026a8ef1249dcdaaa832da789d176b6
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 19:34:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0 
   new_array = []
   
-  while array.length do 
+  while counter < array.length do 
     new_array << array[counter] ** 2 
     counter += 1 
   end
